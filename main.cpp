8c23b1970fa925651519e34742f089574bda7bec
Jack <44131061+ThePixelated@users.noreply.github.com>
2023-08-25 13:10:10 +0700

    Add Blocks, Collision Blocks & More Detail Blocks data
    Adding a Block or bar object and a new collision mechanic to the bars. The object Block now contains more data to store, within the x axis only (currently).

--- main.cpp
+++ main.cpp
@@ -11,45 +11,69 @@ struct Ball {
 
     int styleBall;
 };
+	
+struct AdditionalCoor{
+	int x_coor;
+	int y_coor;
+};
 
-// void EraseDrawBall(Ball &ball_data) {
-// 	COORD coor;
-//     coor.X = ball_data.x_coor + ball_data.ballSpeed;
-// 	coor.Y = ball_data.y_coor + ball_data.ballSpeed;
+struct Block {
+	int xBarCoor;   // Center Point of X
+	int yBarCoor;   // Center Point of Y
+	
+	AdditionalCoor top_bar;     // Mid Point of top left and top right
+	AdditionalCoor bottom_bar;  // Mid Point of bottom left and bottom right
+	AdditionalCoor left_bar;    // Mid Point of top left and bottom left
+	AdditionalCoor right_bar;   // Mid Point of top right and bottom right
+	AdditionalCoor center_bar;  // The Center of the block
+	
+	AdditionalCoor topLeft_bar;
+	AdditionalCoor topRight_bar;
+	AdditionalCoor bottomLeft_bar;
+	AdditionalCoor bottomRight_bar;
+};
 
-// 	SetConsoleCursorPosition(GetStdHandle( STD_OUTPUT_HANDLE ), coor);
-//     std::cout << char(219);
+Block* _BarInnit(int X_axis = -1, int Y_axis = -1, int WidthBlock = 1, int HeightBlock = 5, int BlockRotation = 0, int ManyBlocks = 1, int ScrnWidth = 119, int ScrnHeight = 119) {
+	Block* bars = new Block[size];
+	
+	if (X_axis == -1) X_axis = (ScrnWidth / 2) + 1;
+	if (Y_axis == -1) y_axis = ScrnHeight / 2;
+	
+	for (int i = 0; i < ManyBlock; i++) {
+		bar.xBarCoor = X_axis;
+		bar.yBarCoor = Y_axis;
+		
+		bar.top_bar.x_coor = bar.xBarCoor;
+		bar.bottom_bar.x_coor = bar.xBarCoor;
+		
+		bar.top_bar.y_coor = bar.yBarCoor - ManyBlock;
+		bar.bottom_bar.y_coor = bar.yBarCoor + ManyBlock;
+	}
+	
+	return bars;
+}
 
-// 	coor.X = ball_data.x_coor;
-// 	coor.Y = ball_data.y_coor;
-
-// 	SetConsoleCursorPosition(GetStdHandle( STD_OUTPUT_HANDLE ), coor);
-//     std::cout << ' ';
-// }
-
-void EraseBall(Ball ball_data) {
+void EraseBall(Ball* ball_data, int size) {
     COORD coor;
-    coor.X = ball_data.x_coor;
-	coor.Y = ball_data.y_coor;
-
-	SetConsoleCursorPosition(GetStdHandle( STD_OUTPUT_HANDLE ), coor);
-    std::cout << ' ';
-
+    for (int i = 0; i < size; i++) {
+        coor.X = ball_data[i].x_coor;
+		coor.Y = ball_data[i].y_coor;
+	
+		SetConsoleCursorPosition(GetStdHandle( STD_OUTPUT_HANDLE ), coor);
+	    std::cout << ' ';
+	}
     // Sleep(100);
 }
 
-void RenderBall(Ball ball_data, int index) {    // Remove int index later !!!!!!
+void RenderBall(Ball* ball_data, int size) {
     COORD coor;
-    coor.X = ball_data.x_coor;
-	coor.Y = ball_data.y_coor;
-
-	SetConsoleCursorPosition(GetStdHandle( STD_OUTPUT_HANDLE ), coor);
-    // if (index % 2 == 0) std::cout << char(219);
-    // else std::cout << char(176);
-
-    std::cout << char(ball_data.styleBall);     // Remove this also !!!!!!!!!
-
-    // Sleep(100);
+    for (int i = 0; i < size; i++) {
+        coor.X = ball_data[i].x_coor;
+		coor.Y = ball_data[i].y_coor;
+	
+		SetConsoleCursorPosition(GetStdHandle( STD_OUTPUT_HANDLE ), coor);
+	    std::cout << char(219);
+	}
 }
 
 void BorderCollision(Ball* ball_data, int widthScreen, int heightScreen, int size) {
@@ -70,38 +94,56 @@ void BorderCollision(Ball* ball_data, int widthScreen, int heightScreen, int siz
     }
 }
 
-/*void BallCollision(Ball* ball_data, int size) {
-    for (int i = 0; i < size; i++) {
-        for (int j = 0; j < size; i++) {
-            if (ball_data[i].x_coor <= 0) {
-                ball_data[i].x_borderHit = true;
-            } else if (ball_data[i].x_coor >= widthScreen - 1) {
-                ball_data[i].x_borderHit = false;
-            }
-
-            if (ball_data[i].y_coor <= 0) {
-                ball_data[i].y_borderHit = true;
-            } else if (ball_data[i].y_coor >= heightScreen) {
-                ball_data[i].y_borderHit = false;
-        }
-        }
-    }
-}*/
+void BallCollisionBar(Ball* ball_data, Block bar, int size) {
+	for (int i = 0; i < size; i++) {
+		// X AXIS CHECK
+		// ------------
+		// Left Side Bar Collision
+		if (ball_data[i].x_borderHit && (ball_data[i].x_coor >= bar.xBarCoor && ball_data[i].x_coor <= bar.xBarCoor) && (ball_data[i].y_coor >= bar.top_bar.y_coor && ball_data[i].y_coor <= bar.bottom_bar.y_coor) ) {
+			ball_data[i].x_coor -= 1;
+			ball_data[i].x_borderHit = false;
+		}
+		
+		// Right Side Bar Collision
+		else if (!ball_data[i].x_borderHit && (ball_data[i].x_coor <= bar.xBarCoor && ball_data[i].x_coor >= bar.xBarCoor) && (ball_data[i].y_coor >= bar.top_bar.y_coor && ball_data[i].y_coor <= bar.bottom_bar.y_coor) ) {
+			ball_data[i].x_coor += 1;
+			ball_data[i].x_borderHit = true;
+		}
+		
+		// Y AXIS CHECK
+		// ------------
+		// Top Side Bar Collision
+		
+		// Bottom Side Bar Collision
+	}
+}
 
 void BallMovement(Ball* ball_data, int size) {
     // Ball Movement
     // -------------
     for (int i = 0; i < size; i++) {
-        EraseBall(ball_data[i]);
+//        EraseBall(ball_data[i]);
 
         if (ball_data[i].x_borderHit) ball_data[i].x_coor += ball_data[i].ballSpeed;
         else ball_data[i].x_coor -= ball_data[i].ballSpeed;
 
-        if (ball_data[i].y_borderHit) ball_data[i].y_coor += ball_data[i].ballSpeed;
-        else ball_data[i].y_coor -= ball_data[i].ballSpeed;
+//        if (ball_data[i].y_borderHit) ball_data[i].y_coor += ball_data[i].ballSpeed;
+//        else ball_data[i].y_coor -= ball_data[i].ballSpeed;
 
-        RenderBall(ball_data[i], i);
+//        RenderBall(ball_data[i]);
     }
+}
+
+void RenderBlock(Block bar) {
+    COORD coor;
+    
+    for (int i = bar.top_bar.y_coor; i <= bar.top_bar.y_coor - (bar.top_bar.y_coor - bar.bottom_bar.y_coor); i++) {
+    	coor.X = bar.xBarCoor;
+		coor.Y = i;
+	
+		SetConsoleCursorPosition(GetStdHandle( STD_OUTPUT_HANDLE ), coor);
+		std::cout << char(219);
+	}
 }
 
 void corner_coorBuff(int ScreenWidth, int ScreenHeight) {
@@ -115,34 +157,35 @@ void corner_coorBuff(int ScreenWidth, int ScreenHeight) {
 int main() {
     int width = 119;
     int height = 29;
+	
+	Block bar;
 
-    int size;
+//	_barInnit();
+	
+	int size;
     std::cin >> size;
     Ball ball[size];
 
     for (int i = 0; i < size; i++) {
         std::cin >> ball[i].x_coor >> ball[i].y_coor >> ball[i].ballSpeed >> ball[i].x_borderHit >> ball[i].y_borderHit;
-        ball[i].styleBall = i + 65;
-        // ball[i].x_coor = width / 2;
-        // ball[i].y_coor = height / 2;
-        // ball[i].ballSpeed = 1;
-
-        // ball[i].x_borderHit;
-        // ball[i].y_borderHit;
     }
+    
+	RenderBlock(bar);
 
-    while (true) {
-        BorderCollision(ball, width, height, size);
-        // BallCollision
-        BallMovement(ball, size);
+	while (true) {
+		BorderCollision(ball, width, height, size);
+        BallCollisionBar(ball, bar, size);
         
-        // EraseDrawBall(ball);
-
-        corner_coorBuff(width, height);
-        Sleep(25);
-    }
-
-    _getch();
-
-    return 0;
+		EraseBall(ball, size);
+		BallMovement(ball, size);
+        RenderBall(ball, size);
+		
+		corner_coorBuff(119, 29);
+		Sleep(25);
+	}
+	
+	
+	getch();
+	
+	return 0;
 }
